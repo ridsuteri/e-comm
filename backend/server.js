b6135458a6be2b0a78e79ad4741f4ed51a5bc5db
Riddhi Suteri <ridsuteri@gmail.com>
2021-08-10 11:45:43 +0530

    create signin backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 import express from "express";
 import productRouter from "./routers/productRouter.js";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
 import userRouter from "./routers/userRouter.js";
+
+dotenv.config();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useNewUrlParser: true,
