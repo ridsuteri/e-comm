a8998395ebf9bd4c56475e47a07fa403f76bacd6
Riddhi Suteri <ridsuteri@gmail.com>
2021-08-15 12:07:25 +0530

    create payment screen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -57,6 +57,7 @@ export const signout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
 
   localStorage.removeItem("cartItems");
+  localStorage.removeItem("shippingAddress");
 
   dispatch({ type: USER_SIGNOUT });
 };
